4c86415ab4fbc413a30e7a2e727fd1d0c10c9f71
Greta Schock <gschockcodes@gmail.com>
2021-05-07 16:16:55 -0700

    Changed testing to say "it" instead of "describe"

--- test/0001_two_sum_TEST.js
+++ test/0001_two_sum_TEST.js
@@ -5,7 +5,7 @@ console.log(twoSum)
 
 
 describe("twoSum function", function() {
-    describe("take an array of integers and an integer target and return indices of the two numbers such that they add up to target", function() {
+    it("take an array of integers and an integer target and return indices of the two numbers such that they add up to target", function() {
         var example1 = twoSum([2,7,11,15], 9);
         var example2 = twoSum([3,2,4], 6);
         var example3  = twoSum([3,3], 6); 
