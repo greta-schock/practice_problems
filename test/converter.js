c9b4b47494433a54bd770e7636047e97bbc44c2c
Greta Schock <gschockcodes@gmail.com>
2021-05-07 15:56:33 -0700

    Reorganized testing system for toy problems

--- test/converter.js
+++ test/converter.js
@@ -1,7 +1,7 @@
 //https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
 
 var expect = require("chai").expect;
-var converter = require("../app/converter");
+var converter = require("../app/converter.js");
 
 describe("Color Code Converter", function() {
     //specification code
