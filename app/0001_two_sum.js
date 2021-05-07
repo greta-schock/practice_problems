20ebe55f58b0ebadec89a2aed5ecaa90f02b14cf
Greta Schock <gschockcodes@gmail.com>
2021-05-07 16:18:23 -0700

    Removed rogue console.logs

--- app/0001_two_sum.js
+++ app/0001_two_sum.js
@@ -42,5 +42,3 @@ exports.twoSum = function(nums, target) {
     }
     
 };
-
-// console.log(twoSum([2,7,11,15], 9));
--- test/0001_two_sum_TEST.js
+++ test/0001_two_sum_TEST.js
@@ -1,8 +1,6 @@
 var expect = require("chai").expect;
 var { twoSum } = require("../app/0001_two_sum.js"); 
 
-console.log(twoSum)
-
 
 describe("twoSum function", function() {
     it("take an array of integers and an integer target and return indices of the two numbers such that they add up to target", function() {
