064ae7f80765959e53b9b4732f665f6fe2c278b7
Greta Schock <gschockcodes@gmail.com>
2021-05-09 09:14:11 -0700

    Completed test suite for problem 1108

--- test/1108_defanging_an_IP_address_TEST.js
+++ test/1108_defanging_an_IP_address_TEST.js
@@ -1,2 +1,12 @@
 var expect = require('chai').expect;
 var { defangIPaddr } = require('../app/1108_defanging_an_IP_address');
+
+describe('Leetcode Problem #1108: Defanging an IP Address', function() {
+    it('Given a valid IP address, it should return a defanging version of the IP address', function() {
+        var example1 = defangIPaddr("1.1.1.1")
+        var example2 = defangIPaddr("255.100.50.0")
+
+        expect(example1).to.equal("1[.]1[.]1[.]1");
+        expect(example2).to.equal("255[.]100[.]50[.]0");
+    })
+});
