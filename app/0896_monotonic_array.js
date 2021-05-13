e3fc2d69979da31009eff90da10a59886000ee5f
Greta Schock <gschockcodes@gmail.com>
2021-05-13 13:46:25 -0700

    Added today's prompt

---
+++ app/0896_monotonic_array.js
@@ -0,0 +1,11 @@
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j].  An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Return true if and only if the given array nums is monotonic.
+
+Note
+1. 1 <= nums.length <= 50000
+2. -100000 <= nums[i] <= 100000
+*/
