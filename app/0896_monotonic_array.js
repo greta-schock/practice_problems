a8816ea63d95428b6f4d5dd38e770840455b2aaf
Greta Schock <gschockcodes@gmail.com>
2021-05-13 13:46:59 -0700

    Added examples

--- app/0896_monotonic_array.js
+++ app/0896_monotonic_array.js
@@ -5,6 +5,27 @@ An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j].  An 
 
 Return true if and only if the given array nums is monotonic.
 
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+Example 4:
+
+Input: nums = [1,2,4,5]
+Output: true
+Example 5:
+
+Input: nums = [1,1,1]
+Output: true
+
 Note
 1. 1 <= nums.length <= 50000
 2. -100000 <= nums[i] <= 100000
