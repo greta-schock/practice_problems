e2f8f6915928677fc345c744522099d20b7e9f9e
Greta Schock <gschockcodes@gmail.com>
2021-05-13 13:54:09 -0700

    Added testing to today's problem (#896)

--- test/0896_monotonic_array_TEST.js
+++ test/0896_monotonic_array_TEST.js
@@ -1,2 +1,18 @@
 var { expect } = require('chai');
 var { isMonotonic } = require('../app/0896_monotonic_array');
+
+describe('Leetcode Problem #896: Monotonic Array', function() {
+    it('Return true if an array is monotonically increasing or decreasing', function() {
+        var example1 = isMonotonic([1,2,2,3]);
+        var example2 = isMonotonic([6,5,4,4]);
+        var example3 = isMonotonic([1,3,2]);
+        var example4 = isMonotonic([1,2,4,5]);
+        var example5 = isMonotonic([1,1,1]);
+
+        expect(example1).to.equal(true);
+        expect(example2).to.equal(true);
+        expect(example3).to.equal(false);
+        expect(example4).to.equal(true);
+        expect(example5).to.equal(true);
+    })
+})
