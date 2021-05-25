2de5b66be71afe092a0978d0861008886a67e213
Greta Schock <gschockcodes@gmail.com>
2021-05-25 10:14:24 -0700

    Added today's leetcode problem solution (1832)

--- app/1832_check_if_the_sentence_is_pangram.js
+++ app/1832_check_if_the_sentence_is_pangram.js
@@ -20,3 +20,56 @@ Constraints:
 1 <= sentence.length <= 1000
 sentence consists of lowercase English letters.
 */
+
+/*
+ * @param {string} sentence
+ * @return {boolean}
+ */
+
+var checkIfPangram = function (sentence) {
+
+    if (sentence.length < 26) {
+        return false;
+    }
+
+    var alphabet = {
+        a: false,
+        b: false,
+        c: false,
+        d: false,
+        e: false,
+        f: false,
+        g: false,
+        h: false,
+        i: false,
+        j: false,
+        k: false,
+        l: false,
+        m: false,
+        n: false,
+        o: false,
+        p: false,
+        q: false,
+        r: false,
+        s: false,
+        t: false,
+        u: false,
+        v: false,
+        w: false,
+        x: false,
+        y: false,
+        z: false
+    }
+
+    for (var i = 0; i < sentence.length; i++) {
+        alphabet[sentence[i]] = true;
+    }
+
+    for (var letter in alphabet) {
+        if (!alphabet[letter]) {
+            return false;
+        }
+    }
+
+    return true;
+};
