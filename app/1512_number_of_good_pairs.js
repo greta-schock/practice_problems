48a3ba8b6fa5a84a3bdd703bf0fee8160f1cc4fa
Greta Schock <gschockcodes@gmail.com>
2021-06-02 16:02:04 -0700

    Added 1512's solution

--- app/1512_number_of_good_pairs.js
+++ app/1512_number_of_good_pairs.js
@@ -28,3 +28,15 @@ Constraints:
 1 <= nums.length <= 100
 1 <= nums[i] <= 100
 */
+
+exports.numIdenticalPairs = function (nums) {
+    var counter = 0; 
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length; j++) {
+            if ((nums[i] === nums[j]) && (i < j)){
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
