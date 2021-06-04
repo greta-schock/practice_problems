90442697b3e243fba61569b2cffb13d33e7f8ecf
Greta Schock <gschockcodes@gmail.com>
2021-06-03 23:04:12 -0700

    Added the second problem and test to files

--- app/prep02_takehome_questions.js
+++ app/prep02_takehome_questions.js
@@ -21,6 +21,37 @@ exports.fibonacci_series = fibonacci_series;
 
 var mostCommonString = function (arr) {
     if (arr.length === 0) {
-        return 
+        return null;
     }
-}
+
+    if (arr.length === 1 && typeof arr[0] === 'string') {
+        return arr[0];
+    }
+
+    var ref = {}; 
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'string') {
+            if (ref[arr[i]]) {
+                ref[arr[i]]++;
+            } else {
+                ref[arr[i]] = 1;
+            }
+        }
+    }
+
+    if (Object.keys(ref).length === 0) {
+        return null;
+    }
+
+    var mostCommonStringCount = 0; 
+    var result = '';
+    for (var string in ref) {
+        if (ref[string] > mostCommonStringCount) {
+            mostCommonStringCount = string.length;
+            result = string;
+        }
+    }
+    return string;
+};
+
+exports.mostCommonString = mostCommonString;
--- test/prep02_takehome_questions_TEST.js
+++ test/prep02_takehome_questions_TEST.js
@@ -1,16 +1,30 @@
 var { expect } = require('chai');
-var { fibonacci_series } = require('../app/prep02_takehome_questions');
+var { fibonacci_series, mostCommonString } = require('../app/prep02_takehome_questions');
 
 describe('Takehome #02 Prep Questions', function() {
     it('Should print out n digits of the Fionacci sequence', function() {
-        var example1 = fibonacci_series(-10);
-        var example2 = fibonacci_series(0);
-        var example3 = fibonacci_series(8);
-        var example4 = fibonacci_series(15);
+        var example1_1 = fibonacci_series(-10);
+        var example1_2 = fibonacci_series(0);
+        var example1_3 = fibonacci_series(8);
+        var example1_4 = fibonacci_series(15);
 
-        expect(example1).to.equal(null);
-        expect(example2).to.equal(null);
-        expect(example3).to.eql([0,1,1,2,3,5,8,13,21]);
-        expect(example4).to.eql([0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610])
-    })
-})
+        expect(example1_1).to.equal(null);
+        expect(example1_2).to.equal(null);
+        expect(example1_3).to.eql([0,1,1,2,3,5,8,13,21]);
+        expect(example1_4).to.eql([0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610])
+    });
+
+    it('Should detemine the most common string in an array', function () {
+        var example2_1 = mostCommonString([]);
+        var example2_2 = mostCommonString([1, 2, 3, 4]);
+        var example2_3 = mostCommonString(['1', '12', '123', 1234]);
+        var example2_4 = mostCommonString(['a', 'ab', 'abc', 'abcd', 'abcdef']);
+
+        expect(example2_1).to.equal(null);
+        expect(example2_2).to.equal(null);
+        expect(example2_3).to.equal('123');
+        expect(example2_4).to.equal('abcdef');
+    });
+});
+
+
