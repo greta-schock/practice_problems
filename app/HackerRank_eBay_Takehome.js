e5f2d13d430b3be012a56a24357786975c43d2bc
Greta Schock <gschockcodes@gmail.com>
2021-06-04 15:23:05 -0700

    Added testing for problem two

--- app/HackerRank_eBay_Takehome.js
+++ app/HackerRank_eBay_Takehome.js
@@ -16,20 +16,20 @@ var fibonacciSeq = function (n) {
         return [0, 1];
     } 
 
-    var fib = [];
+    var result = [];
 
-    fib[0] = 0;
-    fib[1] = 1;
+    result[0] = 0;
+    result[1] = 1;
     for (var i = 2; i < n; i++) {
-        fib[i] = fib[i - 2] + fib[i - 1];
+        result[i] = result[i - 2] + result[i - 1];
     }
-    return fib;
+    return result;
 }
 
 exports.fibonacciSeq = fibonacciSeq;
 
 /*
-Given an array with a string(s?) inside, as you remove characters from the beginning of the string, figure out if there are matching prefixes with the leftover
+Given an array of a strings, as you remove characters from the beginning of the string (prefix), return an array of the matching prefixes count per string
 
 '' - 'abcabcd' ---> 7
 'a'- 'bcabcd' ---> 0
@@ -39,20 +39,41 @@ Given an array with a string(s?) inside, as you remove characters from the begin
 'abcab' - 'cd' ---> 0
 'acbabcd' - 'd' ---> 0
 
-Return an array with the total number of matching prefixes [11]
+Return an array with the total number of matching prefixes [10]
 */
 
-var matchingPrefixes = function (arr) {
+//helper function
+var determineIfMatchingPrefixes = function (str) {
+    var arr = str.split('');
+    var counter = str.length;
+    var prefixEndIndex = 0;
+    var currentSubstring = '';
+    while (arr.length > 0) {
+        var strAgain = arr.join('');
+        var equalLengthPrefix = strAgain.substring(0, prefixEndIndex);
+        if (currentSubstring === equalLengthPrefix) {
+            counter += equalLengthPrefix.length;
+        }
+        currentSubstring = currentSubstring + arr[0];
+        arr.shift();
+        prefixEndIndex++;
+    }
+    return counter;
+}
+
+var matchingPrefixesCounter = function (arr) {
     if (arr.length === 0) {
         return [];
     }
-
-    if (arr.length === 1) {
-        return [0];
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        var currentStr = arr[i].toLowerCase();
+        result.push(determineIfMatchingPrefixes(currentStr));
     }
-
-    
+    return result;
 }
+
+exports.matchingPrefixesCounter = matchingPrefixesCounter;
 
 
 
--- test/HackerRank_eBay_Takehome_TEST.js
+++ test/HackerRank_eBay_Takehome_TEST.js
@@ -1,5 +1,5 @@
 var { expect } = require('chai');
-var { fibonacciSeq } = require('../app/HackerRank_eBay_Takehome');
+var { fibonacciSeq, matchingPrefixesCounter } = require('../app/HackerRank_eBay_Takehome');
 
 describe('HackerRank eBay Hacker Rank Questions', function() {
     it('Should print out n digits of the Fionacci sequence', function() {
@@ -15,4 +15,18 @@ describe('HackerRank eBay Hacker Rank Questions', function() {
         expect(example1_4).to.eql([0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]);
         expect(example1_5).to.eql([0, 1, 1, 2]);
     });
+
+    it('Should return an array the count of matching prefixes for a series of substrings generated from a single string', function() {
+        var example2_1 = matchingPrefixesCounter([]);
+        var example2_2 = matchingPrefixesCounter(['abcabcd'])
+        var example2_3 = matchingPrefixesCounter(['a']);
+        var example2_4 = matchingPrefixesCounter(['abcabcd', 'abcabcd', 'abcabcd']);
+        var example2_5 = matchingPrefixesCounter(['abcabcd', 'gretagreat', 'elephant', 'banana']);
+
+        expect(example2_1).to.eql([]);
+        expect(example2_2).to.eql([10]);
+        expect(example2_3).to.eql([1]);
+        expect(example2_4).to.eql([ 10, 10, 10 ]);
+        expect(example2_5).to.eql([ 10, 10, 8, 6 ]);
+    })
 });
