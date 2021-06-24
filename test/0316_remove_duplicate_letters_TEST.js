bde586829cb239f941bf01a080138e24e0659518
Greta Schock <gschockcodes@gmail.com>
2021-06-23 18:37:39 -0700

    Completed testing

--- test/0316_remove_duplicate_letters_TEST.js
+++ test/0316_remove_duplicate_letters_TEST.js
@@ -1,2 +1,12 @@
 var { expect } = require('chai');
 var { removeDuplicateLetters } = require('../app/0316_remove_duplicate_letters');
+
+describe('Leetcode #316: Remove Duplicate Letters', function() {
+    it ('Should remove all duplicates from a string and then put in lexilogical order', function() {
+        var example1 = removeDuplicateLetters('bcabc');
+        var example2 = removeDuplicateLetters('cbacdcbc');
+
+        expect(example1).to.equal('abc');
+        expect(example2).to.equal('abcd');
+    });
+});
